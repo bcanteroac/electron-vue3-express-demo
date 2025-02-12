3bda838e0a7cb9833f96bc1b3041b780339cec0d
bcanteroac <bcanteroac@gmail.com>
2025-02-12 00:29:43 -0300

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import ListComponent from "./pages/ListComponent.vue";
+import ContactComponent from "./pages/ContactComponent.vue"
+
+const routes = [
+    {
+        name: 'list',
+        path: '/',
+        component: ListComponent
+    },
+    {
+        name: 'contact',
+        path: '/contact',
+        component: ContactComponent
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+});
+
+export default router;
