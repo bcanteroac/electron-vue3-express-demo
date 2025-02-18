45f7394d6edb3e2c92d0e07126026db4077f16f4
bcanteroac <bcanteroac@gmail.com>
2025-02-18 00:51:57 -0300

    .

---
+++ items.js
@@ -0,0 +1,9 @@
+const items = [
+    'To Do 1',
+    'To Do 2',
+    'To Do 3',
+    'To Do 4',
+    'To Do 5.1',
+]
+
+module.exports.items = items;
--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const { app, BrowserWindow } = require('electron');
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-      })
+// const { items } = require('./items')
+const db = require('./database')
 
-    win.webContents.loadURL('http://localhost:8080/ ');
-    // win.webContents.openDevTools();
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+
+  win.webContents.loadURL('http://localhost:8080/ ');
+
+  win.webContents.on('did-finish-load', () => {
+    // win.webContents.send('pr-items', items)
+    db.item_all();
+  })
+
+  // win.webContents.openDevTools();
+
+  db.create_db();
+
 }
 
 app.whenReady().then(createWindow);
